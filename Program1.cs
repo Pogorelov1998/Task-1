ab02bbbb4cf122f807c1a31a1df6fd70df397270
Bogdan Pogorelov <boga-rap@mail.ru>
2017-07-05 16:31:14 +0500

    Task 1

---
+++ Program1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Z1P
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            n1 = n2 = 0;
+            Console.Write("Введите два числа: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Среднее арифметическое: ");
+            Console.Write((n1 + n2) / 2.0);
+            Console.Write("\nСреднее геометрическое: ");
+            Console.Write(Math.Sqrt(n1*n2));
+            Console.ReadKey();
+        }
+    }
+}
